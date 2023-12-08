803c9fc7b4bcb9183ba32f354cd8ce175585466c
Spoldma <sander.poldma@gmail.com>
2023-12-08 20:58:16 +0200

    init

---
+++ backend/database.js
@@ -0,0 +1,37 @@
+const Pool = require('pg').Pool;
+
+const pool = new Pool({
+    user: "postgres",
+    password: "parool", //add your password
+    database: "hackaton",
+    host: "localhost",
+    port: "5432"
+});
+
+const execute = async(sql_query) => {
+    try {
+        await pool.connect(); // create a connection
+        await pool.query(sql_query); // executes a query
+        return true;
+    } catch (error) {
+        console.error(error.stack);
+        return false;
+    }
+};
+
+const createTblQuery = `
+    CREATE TABLE IF NOT EXISTS "files" (
+        id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+        course VARCHAR(200) NOT NULL UNIQUE,
+        file_name VARCHAR(200) NOT NULL UNIQUE,
+        description VARCHAR(200) NOT NULL,
+        date_added DATE NOT NULL
+    );`;
+
+execute(createTblQuery).then(result => {
+    if (result) {
+        console.log('Table "files" is created');
+    }
+});
+
+module.exports = pool;
---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const pool = require('./database');
+const cors = require('cors');
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(cors({ origin: 'http://localhost:8080', credentials: true }));
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log("Server is listening to port " + port)
+});
