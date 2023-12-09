ab7fa14257cfe04e7891dbd626df50b7ab1ccdc5
Kristian Tamm <tammkristian@gmail.com>
2023-12-09 14:40:27 +0200

    working file send to REST api

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const multer = require('multer');
 const path = require('path');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 const app = express();
 
 app.use(cors({ origin: 'http://localhost:8080', credentials: true }));
